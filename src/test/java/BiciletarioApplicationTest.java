f16581a716394757cde0933eab8a388a8effee83
Guilherme Menezes <guilhermetav@edu.unirio.br>
2023-06-21 23:07:37 -0300

    Ajustes no projeto

--- src/test/java/BiciletarioApplicationTest.java
+++ src/test/java/BiciletarioApplicationTest.java
@@ -7,13 +7,10 @@ class BicicletarioApplicationTests {
 
     @Test
     void contextLoads() {
-        // Verifica se o contexto da aplicação é carregado corretamente
     }
 
     @Test
     void mainTest() {
-        // Testa a execução do método main
         BicicletarioApplication.main(new String[]{});
-        // Verifica se a aplicação é iniciada sem lançar exceções
     }
 }
